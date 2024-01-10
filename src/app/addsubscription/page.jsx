3ecc95116822dff9600d839ccb506a8af0d069d9
Hashgate <hashgatedev@gmail.com>
2024-01-10 17:20:45 +0400

    changes

---
+++ src/app/addsubscription/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Subscription from '../../components/subscription/page'
+
+const page = () => {
+  return (
+    <>
+
+    <Subscription/>
+    </>
+  )
+}
+
+export default page
